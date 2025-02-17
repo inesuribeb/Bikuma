92a167540eb423c3cb6708a9e7898fe6b113f719
inesuribeb <inesuribeb@gmail.com>
2025-02-17 09:58:49 +0100

    cta

--- mi-aplicacion/src/components/CTA/Cta.jsx
+++ mi-aplicacion/src/components/CTA/Cta.jsx
@@ -1,28 +1,47 @@
 import ServicioCard from './ServicioCard'
+import './Cta.css'
 
 function Cta() {
     return (
-        <div>
-            <ServicioCard
-                braileLogo="/images/Braile.png"
-                title="Tienda online"
-                description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
-            />
-            <ServicioCard
-                braileLogo="/images/Braile2.png"
-                title="Administración Pública"
-                description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
-            />
-            <ServicioCard
-                braileLogo="/images/Braile3.png"
-                title="Fabricamos a medida"
-                description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
-            />
-            <ServicioCard
-                braileLogo="/images/Braile4.png"
-                title="Digitalización accesible"
-                description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
-            />
+        <div className='servicios-main-container'>
+            {/* <h2>Servicios</h2> */}
+            <div className='servicios-container'>
+                <ServicioCard
+                    braileLogo="/images/Braile.png"
+                    title="Tienda online"
+                    description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile2.png"
+                    title="Administración Pública"
+                    description="Nullam facilisis ligula et enim ultrices sagittis Aenean at felis eget"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile3.png"
+                    title="Fabricamos a medida"
+                    description="Aliquam vitae mauris non augue sollicitudin porttitorodio vehicula"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile4.png"
+                    title="Digitalización accesible"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer"
+                />
+                {/* <ServicioCard
+                    braileLogo="/images/Braile5.png"
+                    title="Formación e inclusión"
+                    description="Nullam facilisis ligula et enim ultrices sagittis Aenean at felis eget"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile6.png"
+                    title="Restauración inclusiva"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile7.png"
+                    title="Simulación de entornos 3D"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer "
+                /> */}
+            </div>
         </div>
     )
 }
--- mi-aplicacion/src/components/CTA/ServicioCard.jsx
+++ mi-aplicacion/src/components/CTA/ServicioCard.jsx
@@ -1,9 +1,13 @@
+import './ServicioCard.css'
+
 function ServicioCard ({ braileLogo, title, description }) {
     return (
         <div className="single-servicio-card">
+            <div className='card-info'>
             <img src={braileLogo} alt={title} />
-            <h2>{title}</h2>
+            <h5>{title}</h5>
             <p>{description}</p>
+            </div>
         </div>
     )
 }
