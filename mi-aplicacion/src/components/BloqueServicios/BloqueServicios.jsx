e90efe9ca6c472079e86d292cba66ce3f46a98a7
inesuribeb <inesuribeb@gmail.com>
2025-02-17 10:21:44 +0100

    bloque servicios

--- mi-aplicacion/src/components/BloqueServicios/BloqueServicios.jsx
+++ mi-aplicacion/src/components/BloqueServicios/BloqueServicios.jsx
@@ -1,6 +1,52 @@
+import ServicioCard from "../CTA/ServicioCard";
+import Subtitulo from "../subtitulos/Subtitulo";
+import './BloqueServicios.css'
+
 function BloqueServicios () {
     return (
-        <div></div>
+        <div className='servicios-main-container'>
+            <Subtitulo subtitulo="Servicios" />
+            <div className='servicios-container'>
+                <ServicioCard
+                    braileLogo="/images/Braile.png"
+                    title="Accesibilidad en comunicación"
+                    description="Mauris et turpis tellus. Nullam facilisis, arcu quis lobortis gravida"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile2.png"
+                    title="Servicios de digitalización"
+                    description="Nullam facilisis ligula et enim ultrices sagittis Aenean at felis eget"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile3.png"
+                    title="Diagnósticos y planes estratégicos"
+                    description="Aliquam vitae mauris non augue sollicitudin porttitorodio vehicula"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile4.png"
+                    title="Evacuación y emergencia"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer"
+                />
+
+
+
+                <ServicioCard
+                    braileLogo="/images/Braile5.png"
+                    title="Formación e inclusión"
+                    description="Nullam facilisis ligula et enim ultrices sagittis Aenean at felis eget"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile6.png"
+                    title="Restauración inclusiva"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer"
+                />
+                <ServicioCard
+                    braileLogo="/images/Braile7.png"
+                    title="Simulación de entornos 3D"
+                    description="Donec et nibh facilisis, lobortis lacus non, viverra ante integer "
+                />
+            </div>
+        </div>
     )
 }
 
---
+++ mi-aplicacion/src/components/subtitulos/Subtitulo.jsx
@@ -0,0 +1,11 @@
+import './Subtitulo.css'
+
+function Subtitulo ({subtitulo}) {
+    return (
+        <div className="subtitulo-container">
+            <h2>{subtitulo}</h2>
+        </div>
+    )
+}
+
+export default Subtitulo;
