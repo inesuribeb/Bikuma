68f15afafa5f805cc980f7ae0260751b771eacfc
inesuribeb <inesuribeb@gmail.com>
2025-02-17 13:48:26 +0100

    seccion logotipos

--- mi-aplicacion/src/components/Logotipos/Logotipos.jsx
+++ mi-aplicacion/src/components/Logotipos/Logotipos.jsx
@@ -1,6 +1,25 @@
-function Logotipos () {
+import './Logotipos.css'
+
+function Logotipos() {
+    const logotiposObj = {
+        bizkaiadforal: "/images/logotipos/g10.png",
+        bbva: "/images/logotipos/Group.png",
+        alsa: "/images/logotipos/Vector.png",
+        bilbobus: "/images/logotipos/text14418.png",
+        bizkaibus: "/images/logotipos/g27550.png",
+        vilamuseu: "/images/logotipos/logo-vilamuseu 2.png",
+    };
     return (
-        <div></div>
+        <div className="logotipos-container">
+            {Object.entries(logotiposObj).map(([name, src]) => (
+                <img
+                    key={name}
+                    src={src}
+                    alt={`${name} logo`}
+                    className="logo-image"
+                />
+            ))}
+        </div>
     )
 }
 
