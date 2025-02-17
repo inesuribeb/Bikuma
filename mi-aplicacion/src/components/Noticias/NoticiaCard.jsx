7922b8fec329790cad67a84d4848722fce1f2b53
inesuribeb <inesuribeb@gmail.com>
2025-02-17 13:23:10 +0100

    seccion proyectos

--- mi-aplicacion/src/components/Noticias/NoticiaCard.jsx
+++ mi-aplicacion/src/components/Noticias/NoticiaCard.jsx
@@ -1,12 +1,27 @@
 import './NoticiaCard.css'
 
-function NoticiaCard({ img, title, newsDescription }) {
+function NoticiaCard({ img, title, newsDescription, isProject }) {
     return (
             <div className="noticiaCard-container">
                 <img src={img} alt={title} />
                 <div className="noticiaCard-info">
-                    <h5>{title}</h5>
-                    <p>{newsDescription}</p>
+                {isProject ? (
+                    <>
+                        <p>{newsDescription}</p>
+                        <div className="location-title">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                <circle cx="12" cy="10" r="3"></circle>
+                            </svg>
+                            <h5>{title}</h5>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <h5>{title}</h5>
+                        <p>{newsDescription}</p>
+                    </>
+                )}
                 </div>
             </div>
     )
--- mi-aplicacion/src/components/Proyectos/Proyectos.jsx
+++ mi-aplicacion/src/components/Proyectos/Proyectos.jsx
@@ -1,6 +1,58 @@
+import { useEffect } from "react";
+import Subtitulo from "../subtitulos/Subtitulo";
+import NoticiaCard from "../Noticias/NoticiaCard";
+import './Proyectos.css'
+
 function Proyectos () {
+    useEffect(() => {
+        const container = document.querySelector('.all-projects-container');
+        const centerElement = container.children[Math.floor(container.children.length / 2)];
+        
+        if (centerElement) {
+            const scrollTo = centerElement.offsetLeft - (container.offsetWidth / 2) + (centerElement.offsetWidth / 2);
+            container.scrollTo({
+                left: scrollTo,
+                behavior: 'smooth'
+            });
+        }
+    }, []);
     return (
-        <div></div>
+        <div className="proyectos-main-container">
+            <Subtitulo subtitulo="Proyectos de accesibilidad universal" />
+            <div className="all-projects-container">
+                <NoticiaCard
+                    img="/images/proyectos/proyecto1.png"
+                    title="Bilbao"
+                    newsDescription="Manual de señalización accesible para un mundo más inclusivo"
+                    isProject={true}
+                />
+                <NoticiaCard
+                    img="/images/proyectos/proyecto2.jpeg"
+                    title="Bilbao"
+                    newsDescription="La estrategia integral para el fomento de la accesibilidad en bilbobus"
+                    isProject={true}
+                />
+                <NoticiaCard
+                    img="/images/proyectos/proyecto3.jpeg"
+                    title="Bilbao"
+                    newsDescription="Alhóndiga bilbao – centro azkuna, pensando en la accesibilidad para todos"
+                    isProject={true} 
+                />
+                <NoticiaCard
+                    img="/images/proyectos/proyecto5.jpeg"
+                    title="Vila Joiosa "
+                    newsDescription="Vilamuseu «museos para todos»"
+                    isProject={true}
+                />
+                <NoticiaCard
+                    img="/images/proyectos/proyecto6.jpeg"
+                    title="Turismo"
+                    newsDescription="¿Tienes cita en Fitur? La accesibilidad estará en tu agenda"
+                    isProject={true}
+                />
+
+            </div>
+        </div>
     )
 }
 
